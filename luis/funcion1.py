63e588dce56085af10e19e63103feb219d642d61
209 <209@academicos.cesde.edu.co>
2022-09-24 11:38:06 -0500

    se realizo funcion saludar

--- luis/funcion1.py
+++ luis/funcion1.py
@@ -1,1 +1,11 @@
 #funciones tradicionales con python
+
+#Declaracacion de una funcion
+def saludar(nombre):
+    return(
+    print(f'hola {nombre}')
+    )
+
+#llamar o invocar la funcion
+nombre="luis"
+saludar(nombre)
