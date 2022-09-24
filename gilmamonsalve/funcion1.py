8f77bb4f00c15cfd96d3a0df47bb7a887ef1d437
209 <209@academicos.cesde.edu.co>
2022-09-24 11:35:36 -0500

    NT se crea la 1era funcion

--- gilmamonsalve/funcion1.py
+++ gilmamonsalve/funcion1.py
@@ -1,1 +1,14 @@
 #funciones tradicionales con python
+#programacion funcional
+#def=funcion
+#principios solid
+#pruebas unitarias
+
+#declaracion de una funcion
+def saludar(nombre):
+    print(f'hola {nombre}')
+
+
+#llamar o invocar la funcion
+nombre="gilma"
+saludar(nombre)
