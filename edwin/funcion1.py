709de2118a699e7dafe138f26551331a0f072d0a
David Osorio <daviosood@gmail.com>
2022-09-24 11:41:20 -0500

    NTS8 funcion saludar

--- edwin/funcion1.py
+++ edwin/funcion1.py
@@ -1,1 +1,8 @@
 # funciones tradicionales con python
+# declaracion de una funcion
+def saludar(nombre):
+    print(f'hola {nombre}')
+
+# llamar o invocar la funcion
+nombre="edwin"
+saludar(nombre)
