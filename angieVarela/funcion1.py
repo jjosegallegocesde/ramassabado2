1c05689370cfb145785e4e0c26eda691116ae674
lemys.almanza@gmail.com <lemys.almanza@gmail.com>
2022-09-24 11:37:15 -0500

    NTS segundo cambio rama AngieVarela

--- angieVarela/funcion1.py
+++ angieVarela/funcion1.py
@@ -1,2 +1,10 @@
 #funciones tradicionales con python
 
+#Declaracion de una funcion
+def saludar(nombre):
+    print(f'Hola: {nombre}')
+
+
+#Llamar o invocar la funcion
+nombre="Angie"
+saludar(nombre)
