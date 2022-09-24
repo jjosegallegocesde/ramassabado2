2cf68fd28d730c3c08a2d072e2178940650785a6
mateo1761 <mateoquinterozapata@gmail.com>
2022-09-24 11:34:19 -0500

    Se crea funcion saludar

--- mateoQuintero/funcion1.py
+++ mateoQuintero/funcion1.py
@@ -1,1 +1,10 @@
 #funciones tradicionales con python
+
+#Declaracion de una funcion en python
+def saludar(nombre):
+    print(f'Hola {nombre}')
+
+#Llamar funcion
+
+nombre = 'mateo'
+saludar(nombre)
