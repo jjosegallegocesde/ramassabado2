826d4ab2a000273ec3bbe6d5510edfa1135b315b
dyruiz1 <dyruiz@gmail.com>
2022-09-24 11:37:32 -0500

    fgjflgjf

--- DeisyRuiz/funcion1.py
+++ DeisyRuiz/funcion1.py
@@ -1,1 +1,10 @@
 #funciones tradicionales con python
+#palabra reservada def
+
+
+#declaracion de una funcion
+def saludar(nombre):
+    print(f'hola {nombre}')
+
+#llamando la funcion en la zona comun debo mandar el dato especifico o mandar antes el dato
+saludar("DeisyRuiz")
