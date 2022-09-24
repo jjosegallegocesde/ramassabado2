0f7e22b1d8244f865c8f506ad6f1dd0acf8ebc30
209 <209@academicos.cesde.edu.co>
2022-09-24 11:37:23 -0500

    ok

--- julianquiros/funcion.py
+++ julianquiros/funcion.py
@@ -1,1 +1,9 @@
-"Funciones tradicionales con python"
+#Funciones tradicionales con python
+#Declaracion de una funcion
+
+def saludar(nombre):
+    print(f'Hola {nombre}')
+    
+#LLamar o invocar la funcion
+nombre = "julian"
+saludar(nombre)
