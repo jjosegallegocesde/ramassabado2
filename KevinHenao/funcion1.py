ede305ea209dcd1a2a023b08e1442be0df7de074
209 <209@academicos.cesde.edu.co>
2022-09-24 11:37:46 -0500

    OK

--- KevinHenao/funcion1.py
+++ KevinHenao/funcion1.py
@@ -1,1 +1,9 @@
 #Funcion tradicionales con python
+
+#Declaracion de una funcion 
+def saludar(nombre):
+    print(f'Hola {nombre}')
+
+#Llamar o invocar la funcion
+nombre = "Kevin"
+saludar(nombre)
